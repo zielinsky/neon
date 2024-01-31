d2f0a1ad34a1b612192956e69fe124796286ef6b
Tagada14 <jan@buszka.eu>
2024-01-31 13:27:33 +0100

    Adjust main

--- bin/main.ml
+++ bin/main.ml
@@ -5,8 +5,7 @@ let print (term, tp) =
     (PrettyPrinter.term_to_string term ^ ": " ^ PrettyPrinter.term_to_string tp)
 
 let _ =
-  List.iter print
-    (List.map (TypeChecker.infer_type env) (Parser.parse_file "TestFile.neon"))
+  List.iter (fun x ->  print (TypeChecker.infer_type env x)) (Parser.parse_file "TestFile.neon")
 
 (* let term, tp = TypeChecker.infer_type env (List.hd (Parser.parse_file "TestFile.neon"))
    let term_s = PrettyPrinter.term_to_string term
