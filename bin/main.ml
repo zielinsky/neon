0914f5b4906e21874b923ea0722040d311385154
Patryk Zieliński <patryk.zielinski@brandmed.pl>
2025-01-04 18:07:09 +0100

    change main neon code

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,44 @@
-let env = Env.create_env ()
+open Arg
 
-let _ =
-  List.iter
-    (fun x ->
-      PrettyPrinter.print_def x;
-      PrettyPrinter.print (TypeChecker.infer_type env x))
-    (Parser.parse_file "eq.neon")
+(* A reference to store the file name provided by the user *)
+let file = ref ""
 
-(* let term, tp = TypeChecker.infer_type env (List.hd (Parser.parse_file "TestFile.neon"))
-   let term_s = PrettyPrinter.term_to_string term
-   let tp_s = PrettyPrinter.term_to_string tp
-   let _ = print_endline (term_s ^ ": " ^ tp_s) *)
+(* A reference to track whether we should print definitions verbosely *)
+let verbose_mode = ref false
+
+(* A specification list for possible command line options *)
+let speclist = [
+  ("-verbose", Set verbose_mode, "Enable verbose printing of definitions");
+]
+
+(* Usage message that will be displayed if the user does not provide valid arguments *)
+let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [-verbose] <file>"
+
+let main () =
+  (* Parse the command line arguments according to speclist.
+     Any non-option argument is handled by the anonymous function (fun s -> file := s). *)
+  Arg.parse
+    speclist
+    (fun s -> file := s)
+    usage_msg;
+
+  (* Check if a file was provided. If not, print usage and exit. *)
+  if !file = "" then begin
+    prerr_endline "No file specified. Please provide a file path.";
+    Arg.usage speclist usage_msg;
+    exit 1
+  end;
+
+  (* Create the environment and process the file *)
+  let env = Env.create_env () in
+  Parser.parse_file !file
+  |> List.iter (fun x ->
+       (* Only print definitions if verbose mode is enabled *)
+       if !verbose_mode then
+         PrettyPrinter.print_def x;
+       (* Always print the inferred type *)
+       PrettyPrinter.print (TypeChecker.infer_type env x)
+     )
+
+(* Entry point *)
+let () = main ()
